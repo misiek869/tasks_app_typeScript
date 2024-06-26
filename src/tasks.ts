bdff1252a1b6620e2ac7abfa061604675397c387
Michal Gawik <gawlik.zator@o2.pl>
2024-06-26 09:59:18 +0200

    add functionality

---
+++ src/tasks.ts
@@ -0,0 +1,58 @@
+const taskFrom = document.querySelector<HTMLFormElement>('.form')
+const fromInput = document.querySelector<HTMLInputElement>('.form-input')
+const taskListElement = document.querySelector<HTMLUListElement>('.list')
+
+type Task = {
+	description: string
+	isCompleted: boolean
+}
+
+const tasks: Task[] = loadTask()
+
+tasks.forEach(renderTask)
+
+function loadTask(): Task[] {
+	const storedTasks = localStorage.getItem('tasks')
+	return storedTasks ? JSON.parse(storedTasks) : []
+}
+
+taskFrom?.addEventListener('submit', e => {
+	e.preventDefault()
+	const taskDescription = fromInput?.value
+
+	if (taskDescription) {
+		const task: Task = {
+			description: taskDescription,
+			isCompleted: false,
+		}
+
+		// add task to list
+		addTask(task)
+
+		// render tasks
+		renderTask(task)
+
+		// update ls
+		updateStorage()
+
+		fromInput.value = ''
+		return
+	}
+	alert('Please enter Your task')
+})
+
+function addTask(task: Task): void {
+	tasks.push(task)
+}
+
+function renderTask(task: Task): void {
+	const taskElement = document.createElement('li')
+	taskElement.textContent = task.description
+	if (taskListElement) {
+		taskListElement.appendChild(taskElement)
+	}
+}
+
+function updateStorage(): void {
+	localStorage.setItem('tasks', JSON.stringify(tasks))
+}
